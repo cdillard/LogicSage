203ab186d59c58b95c29d68a0e116736333ce77a
Chris Dillard <chris.b.dillard@gmail.com>
2023-04-28 06:20:11 -0600

    add star trek simulator

---
+++ Swifty-GPT/StarTrekSimuilator.swift
@@ -0,0 +1,41 @@
+//
+//  StarTrekSimuilator.swift
+//  Swifty-GPT
+//
+//  Created by Chris Dillard on 4/28/23.
+//
+
+import Foundation
+let laForge = """
+Geordi :
+"You know, I've always thought technology could solve almost any problem. It enhances the quality of our lives, lets us travel across the galaxy, even gave me my vision. But sometimes you just have to turn it all off."
+    to holo-Leah; on the solution to their problem
+    TNG : Booby Trap
+Bochra :
+"And they let you live?"
+Geordi :
+"Of course they let me live! What kind of question is that?"
+
+On Geordi's parents reaction to his being born blind.
+    TNG : The Enemy
+Kurn :
+"How long has the bird been dead? It appears to have been lying in the sun for quite some time."
+Geordi :
+"It's not dead, it's been replicated. You do understand that we cook most of our foods?"
+Kurn :
+"Ah, yes... I was told to prepare for that. I shall try some of your burned replicated bird meat."
+    TNG : Sins of the Father
+Geordi :
+"Maybe I'm not make myself clear, Guinan. Barclay, well, he's always late. The man's nervous. Nobody wants to be around this guy."
+Guinan :
+"If I felt that nobody wanted to be around me, I'd probably be late and nervous too."
+Geordi :
+"Guinan, that's not the point!"
+Guinan :
+"Are you sure?"
+    TNG : Hollow Pursuits
+Barclay :
+"Being afraid all the time, of forgetting somebody's name, not knowing what to do with your hands. I mean, I'm the guy who writes down things to remember to say when there's a party. And then when he finally get there, he winds up alone in the corner trying to look comfortable examining a potted plant."
+Geordi :
+"You're just shy, Barclay!"
+"""
